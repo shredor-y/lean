d20ce1ceea66115c6049a3216060a6c80f094953
Seva2000 <seva.g.99@inbox.ru>
2020-06-21 14:18:25 +0500

    gitClone

---
+++ assets/build/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.nav-btn').on('click', function(){
+        $('.menu').addClass('active')
+    })
+    $('.menu_btn').on('click', function(){
+        $('.menu').removeClass('active')
+    })
+})
